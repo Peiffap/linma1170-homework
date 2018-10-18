db0932cec9bdbe4f0b2aa3fe2436fbbd9ed3362d
Gilles Peiffer <gilles.peiffer@outlook.com>
2018-10-19 00:20:13 +0200

    [homework-1/mysolve.py] fix QR(A), start QRSolve
    Finish the code for QR(A), it returns the correct value
    for the example matrix on Wikipedia.
    The QRSolve function still needs work,
    for some reason it fails on a trivial system.

--- homework-1/src/mysolve.py
+++ homework-1/src/mysolve.py
@@ -11,12 +11,13 @@ SolverType = 'scipy'
 
 import scipy.sparse
 import scipy.sparse.linalg
-import numpy
+import numpy as np
 
 def mysolve(A, b):
     if SolverType == 'scipy':
         return True, scipy.sparse.linalg.spsolve(A, b)
     elif SolverType == 'QR':
+        return True, QRSolve(A, b)
         # TODO write QR solver code
     #elif SolverType == 'LU':
         # write here your code for the LU solver
@@ -42,18 +43,59 @@ def QR(A):
            
         Both V and R are NumPy arrays.
     """
+    m = len(A)
+    n = len(A[0])
+    A = np.array(A, dtype=float)
     V = []  # Stores the different reflection vectors
     
-    e1 = np.zeros(n)  # Unit vector
-    e1[0] = 1
+    def find_unit_vector(l):
+        e1 = np.reshape(np.zeros(l), (l, 1))  # Unit vector
+        e1[0] = 1
+        return e1
     
     for k in range(n):
-            x = np.array(A[k:m][k])
-            vk = np.sign(x[0])*np.linalg.norm(x)*e1 + x
-            vk = vk/np.linalg.norm(vk)
-            V[k] = np.array(vk)
-            A[k:m][k:n] -= 2*vk*(np.transpose(vk)*A[k:m][k:n])
+        V.append([])
+        x = np.reshape(A[k:m, k], (m-k, 1))
+        vk = np.sign(x[0])*np.linalg.norm(x)*find_unit_vector(m-k) + x
+        vk = vk/np.linalg.norm(vk)
+        V[k] = vk
+        A[k:m, k:n] = A[k:m, k:n] - 2.*np.dot(vk, np.dot(np.transpose(vk), A[k:m, k:n]))
+
+    return V, A
+
+
+def QRSolve(A, b):
+    m = len(A)
+    n = len(A[0])
+    b = np.array(b, dtype=float)
     
-    return V, R
+    V, R = QR(A)
+    x = np.reshape(np.zeros(n), (n, 1))
+    
+    def vecmatsum(xvec, Rmat, j):
+        sum = 0
+        for k in range(j+1, n):
+            sum += xvec[k] * Rmat[j][k]
+            
+        return sum
+    
+    print(b)
+    
+    for k in range(n):
+        b[k:m] = b[k:m] - 2.*np.dot(V[k], np.dot(np.transpose(V[k]), b[k:m]))
+    
+    print(b)
+    
+    for j in range(n):
+        x[j] = (b[j] - vecmatsum(x, R, j))/R[j][j]
+        
+    return x
+
+
+A = [[1., 1.], [0., 1.]]
+b = [[2.], [1.]]
+
+print(QRSolve(A, b))
+print(scipy.sparse.linalg.spsolve(A, b))
             
         
