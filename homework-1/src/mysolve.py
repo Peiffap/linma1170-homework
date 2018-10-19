7ea3f3c0e5ab8fe61e91e159dd53f01236671a93
Gilles Peiffer <gilles.peiffer@outlook.com>
2018-10-19 20:10:14 +0200

    [mysolve.py] finish qr and qrsolve
    For some reason this still doesn't work even though the returned array
    is basically equal to scipy's...

--- homework-1/src/mysolve.py
+++ homework-1/src/mysolve.py
@@ -7,7 +7,7 @@
 # -clscale 10 coarse mesh
 
 
-SolverType = 'scipy'
+SolverType = 'QR'
 
 import scipy.sparse
 import scipy.sparse.linalg
@@ -18,7 +18,6 @@ def mysolve(A, b):
         return True, scipy.sparse.linalg.spsolve(A, b)
     elif SolverType == 'QR':
         return True, QRSolve(A, b)
-        # TODO write QR solver code
     #elif SolverType == 'LU':
         # write here your code for the LU solver
     #elif SolverType == 'GMRES':
@@ -46,56 +45,84 @@ def QR(A):
     m = len(A)
     n = len(A[0])
     A = np.array(A, dtype=float)
-    V = []  # Stores the different reflection vectors
+    V = np.zeros((m, n))  # Stores the different reflection vectors
     
     def find_unit_vector(l):
-        e1 = np.reshape(np.zeros(l), (l, 1))  # Unit vector
+        e1 = np.zeros(l)  # Unit vector
         e1[0] = 1
         return e1
     
-    for k in range(n):
-        V.append([])
-        x = np.reshape(A[k:m, k], (m-k, 1))
-        vk = np.sign(x[0])*np.linalg.norm(x)*find_unit_vector(m-k) + x
+    def new_sign(x):
+        if x == 0:
+            return 1
+        return np.sign(x)
+    
+    for k in range(n-1):
+        x = A[k:m, k]
+        vk = new_sign(x[0])*np.linalg.norm(x)*find_unit_vector(len(x)) + x
         vk = vk/np.linalg.norm(vk)
-        V[k] = vk
-        A[k:m, k:n] = A[k:m, k:n] - 2.*np.dot(vk, np.dot(np.transpose(vk), A[k:m, k:n]))
-
+        vk.shape = (vk.shape[0], 1)
+        A[k:m, k:n] = A[k:m, k:n] - 2.* vk * (vk.T @ A[k:m, k:n])
+        vk.shape = vk.shape[0]
+        V[k:m, k] = vk
+        
     return V, A
 
 
 def QRSolve(A, b):
+    """
+    This function calculates the solution of a linear system of equations
+    given under the form Ax = b. It uses QR factorization
+    with Householder reflectors to factorize the matrix A,
+    then uses the reflectors stored in V,
+    as well as the upper-triangular matrix R,
+    to back-substitute and find x.
+    
+    Input:
+        A: the first matrix in Ax = b
+        B: the second matrix in Ax = b
+        
+    Output:
+        x: the solution to Ax = b, namely x = A^{-1}b = R^{-1}Q*b.
+    """
     m = len(A)
     n = len(A[0])
     b = np.array(b, dtype=float)
     
     V, R = QR(A)
-    x = np.reshape(np.zeros(n), (n, 1))
+    
+    V = np.array(V, dtype=float)
+    
+    x = np.zeros((n, 1))
     
     def vecmatsum(xvec, Rmat, j):
         sum = 0
-        for k in range(j+1, n):
+        for k in range(j+1, len(xvec)):
             sum += xvec[k] * Rmat[j][k]
             
         return sum
     
-    print(b)
-    
     for k in range(n):
-        b[k:m] = b[k:m] - 2.*np.dot(V[k], np.dot(np.transpose(V[k]), b[k:m]))
+        vk = np.reshape(V[k:m, k], (m-k, 1))
+        b[k:m] = b[k:m] - 2.* vk @ (np.transpose(vk) @ b[k:m])
+        
     
-    print(b)
-    
-    for j in range(n):
+    for i in range(n):
+        j = n-i-1
         x[j] = (b[j] - vecmatsum(x, R, j))/R[j][j]
         
-    return x
+    return np.reshape(x, (1, len(A[0]))).flatten()
 
 
-A = [[1., 1.], [0., 1.]]
-b = [[2.], [1.]]
+A = np.random.rand(1000, 1000)
+b = np.random.rand(1000, 1)
 
-print(QRSolve(A, b))
-print(scipy.sparse.linalg.spsolve(A, b))
+R1 = QRSolve(A, b)
+R2 = scipy.sparse.linalg.spsolve(A, b)
+
+print(np.allclose(R1, R2, 1e-5, 1e-5))
+
+#print("QRSolve:", , "\n")
+#print("SciPy:", , "\n")
             
         
