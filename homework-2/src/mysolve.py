a93f8a5fd8e14b21e1e9b22e8f2d4a07d408a58a
Gilles Peiffer <gilles.peiffer@outlook.com>
2018-11-09 10:37:42 +0100

    [homework-2] Update ccore, mysolve; add report

--- homework-2/src/mysolve.py
+++ homework-2/src/mysolve.py
@@ -1,135 +1,53 @@
-# The function mysolve(A, b) is invoked by ccore.py
-# to solve the linear system
-# Implement your solver in this file and then run:
-# python ccore.py -clscale float
-# where the line argument 'clscale' allows the global coarsening of the mesh
-# -clscale 1  fine mesh
-# -clscale 10 coarse mesh
-
+#The function mysolve(A, b) is invoked by ccore.py
+#to solve the linear system
+#Implement your solver in this file and then run:
+#python ccore.py -clscale float
+#where the line argument 'clscale' allows the global coarsening of the mesh
+#-clscale 1  fine mesh
+#-clscale 10 coarse mesh
 
 # Homework 2 : SVD and condition number
 # @author Gilles Peiffer, 24321600
 # gilles.peiffer@student.uclouvain.be
-# Nov. 9, 2018
+# Nov. 12, 2018
 
 
 SolverType = 'scipy'
 
+import scipy.sparse
+import scipy.sparse.linalg
 import numpy as np
-import scipy
 import matplotlib.pyplot as plt
-from timeit import default_timer as timer
-import csv # for analysis
 
-def mysolve(A, b, clscale):
+def mysolve(A, b):
     if SolverType == 'scipy':
-        cond_anal(A, clscale)
-        return True, scipy.sparse.linalg.spsolve(A, b)  # Needs import
-    #elif SolverType == 'QR':
-        # return True, QRsolve(A, b)
-    #elif SolverType == 'LU':
-        # write here your code for the LU solver
-    #elif SolverType == 'GMRES':
-        # write here your code for the LU solver
+        i, sol = low_rank_approx(A,b)
+        return True, scipy.sparse.linalg.spsolve(A, b), i
+    elif SolverType == 'QR':
+        return True, QRsolve(A, b)
     else:
         return False, 0
-     
-
-def cond_anal(A, clscale):
-    if clscale == 1:
-        wmode = 'w'
-    else:
-        wmode = 'a'
-        
-    with open('cond_anal_clscale.csv', mode=wmode) as dat:
-        dat_writer = csv.writer(dat, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        dat_writer.writerow([clscale, np.linalg.cond(A)])
-
 
 def low_rank_approx(A,b):
-    U, s, Vh = scipy.linalg.svd(A)
-    ensum = 0
-    for i in range(len(s)):
-        ensum += np.dot(b, (np.outer(U.T[i], Vh[i])/s[i]))
-    return ensum
-
-
-def cond_plot():
-    """
-    This function plots graphs that make it possible to easily analyse
-    the impact of different variations to the model of the coil in ccore.py.
-    Data is written from the file written in the previous functions
-    
-    
-    if precision == 'test':
-        N = range(20, 400, 20)
-    elif precision == 'report':
-        N = range(20, 1000, 10)
-    T = []
-    V = []
-    for n in N:
-        A = np.random.rand(n, n)  # Random matrix
-        b = np.random.random(n)  # Random vector
-        start_time_QR = timer()
-        QRsolve(A,b)
-        t_QR = timer()-start_time_QR
-        T.append(t_QR)
-        start_time_NumPy = timer()
-        np.linalg.solve(A,b)
-        t_NumPy = timer()-start_time_NumPy
-        V.append(t_NumPy)
-        
-    logN = np.log(N)
-    logT = np.log(T)
-    logV = np.log(V)
-        
-    # Linear regression
-    fit_QR = np.polyfit(logN, logT, 1)
-    fit_NumPy = np.polyfit(logN, logV, 1)
-    fit_fn_QR = np.poly1d(fit_QR)
-    fit_fn_NumPy = np.poly1d(fit_NumPy)
-    
-    # Cubic fit
-    fit_QR3 = np.polyfit(N, T, 3)
-    fit_fn_QR3 = np.poly1d(fit_QR3)
-    
-    # Logarithmic plot of QRsolve execution time
-    plt.plot(logN, logT, 'bo', logN, fit_fn_QR(logN), '--b')
-    plt.xlabel(r"$\log(n)$")
-    plt.ylabel(r"$\log(t)$")
-    plt.title(r"Execution time of QRsolve on a logarithmic scale"
-              "\n"
-              r"as a function of $n$, the size of $A$")
-    plt.show()
-    
-    # Logarithmic plot of NumPy execution time
-    plt.plot(logN, logV, 'ro', logN, fit_fn_NumPy(logN), '--r')
-    plt.xlabel(r"$\log(n)$")
-    plt.ylabel(r"$\log(t)$")
-    plt.title(r"Execution time of np.linalg.solve on a logarithmic scale"
-              "\n"
-              r"as a function of $n$, the size of $A$")
-    plt.show()
-    
-    # Regular plot of QRsolve execution time
-    plt.plot(N, T, 'go', N, fit_fn_QR3(N), '--g')
-    plt.title(r"Execution time of QRsolve"
-              "\n"
-              r" as a function of $n$, the size of $A$")
-    plt.xlabel(r"$n$")
-    plt.ylabel(r"$t$")
-    plt.show()
-    """
-    
-    with open('cond_anal_clscale.csv') as clres:
-        clres_reader = csv.reader(clres, delimiter=',')
-        clscale = []
-        cond = []
-        for row in clres_reader:
-            clscale.append(row[0])
-            cond.append(row[1])
-            
-    
-
-if __name__ == "__main__":
-    cond_plot()
+     U,s,v = np.linalg.svd(A)
+     #print(s[len(s)-1])
+     #print(s[len(s)-2])
+     sol = 0
+     for i in range(len(s)):
+         sol += np.dot(b,(np.outer(U.T[i], v[i])/s[i]))
+     pourcentage = 0
+     i =1
+     sol2=0
+     while i < len(sol) and pourcentage < 0.9 :
+         sol2+=np.dot(b,(np.outer(U.T[len(sol)-i], v[len(sol)-i])/s[len(sol)-i]))
+         pourcentage = (np.linalg.norm(sol) - np.linalg.norm(sol-sol2))/np.linalg.norm(sol)
+         first = np.dot(b,(np.outer(U.T[len(sol)-1], v[len(sol)-1])/s[len(sol)-1]))
+         first1 =  (np.linalg.norm(sol) - np.linalg.norm(sol-first))/np.linalg.norm(sol)
+         prc = np.linalg.norm(np.dot(b,(np.outer(U.T[len(sol)-1], v[len(sol)-1]/s[len(sol)-1]))))/np.linalg.norm(sol)
+         
+         i+=1
+     #print(first1)
+     #print(prc)
+     #print(i)
+     #print(i-1)
+     return first1, sol
--- homework-2/src/sim.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-export PYTHONPATH="$PYTHONPATH:/Users/admin/Documents/gmsh-git-MacOSX-SDK/lib"
-python ccore.py -clscale 21
-#for i in {1..50}
-#do
-#    python ccore.py -clscale $i
-#done
